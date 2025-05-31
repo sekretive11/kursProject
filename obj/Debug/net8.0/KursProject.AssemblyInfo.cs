2ff089ab0ea050e69b5f3bb061e3e5502e9fd4c0
Секретный <vohedoq13@gmail.com>
2025-06-01 01:18:40 +0300

    NuGet to net6.. Again...

--- obj/Debug/net8.0/KursProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/KursProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KursProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbae771db0932769a2ff08bc40aff7f14995158c")]
 [assembly: System.Reflection.AssemblyProductAttribute("KursProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KursProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
