01296005c418cd6206ce80e5ebf82101c73fa7b6
Секретный <vohedoq13@gmail.com>
2025-06-02 04:36:22 +0300

    Fixing DB 7.0

--- Data/kursProjContext.cs
+++ Data/kursProjContext.cs
@@ -8,12 +8,11 @@ namespace KursProject.Data
     public class kursProjContext : DbContext
     {
         public kursProjContext(DbContextOptions<kursProjContext> options) : base(options) { }
-        public DbSet<MarketingData> marketingdata { get; set; }
+        public DbSet<MarketingData> MarketingData { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<MarketingData>()
-                .ToTable("marketingdata");
+            modelBuilder.Entity<MarketingData>().ToTable("marketingdata");
         }
 
     }
